875b745229b9adb45010f39c7820a8c7e4841e42
Luccasdsm <luccasdsm@hotmail.com>
2025-03-11 20:08:03 -0300

    ajuste no envirmont

--- src/app/core/environments/environment.prod.ts
+++ src/app/core/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiMedium: 'https://medium-api-production-f6f2.up.railway.app/api/'
+  apiMedium: 'https://medium-api-production-f6f2.up.railway.app/'
 };
